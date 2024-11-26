980fb7c75e6680264ad6cf88daef85fd47bd5892
Hishioka Daichi <1te19117r@s.kyushu-u.ac.jp>
2024-11-27 05:06:19 +0900

    about ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,7 @@ try:
 except ImportError:
     DEBUG = False
     SECRET_KEY = get_random_secret_key()
-    ALLOWED_HOSTS = ['.pythonanywhere.com']
+    ALLOWED_HOSTS = ['.pythonanywhere.com','hishi.pythonanywhere.com']
 
 
 
